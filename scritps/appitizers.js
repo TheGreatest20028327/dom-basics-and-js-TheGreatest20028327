2f0e8ea3cff9bd41297d7e8bfdc06220f1474f56
TheGreatest20028327 <2002837@saultcollege.ca>
2023-11-22 19:26:51 -0500

    Debugging attempt

--- scritps/appitizers.js
+++ scritps/appitizers.js
@@ -12,67 +12,79 @@ const Appitizers = () => {
   const imagesContainer = document.createElement('div');
   imagesContainer.setAttribute('class', 'images-container');
 
-  const appItems = () => {
-    const list = document.createElement('li');
-    list.setAttribute('class', 'nav-item list-unstyled mx-2');
+  const item1Container = document.createElement('div');
+  item1Container.setAttribute('class', 'item-container');
 
-    const app1 = document.createElement('h1');
-    app1.setAttribute('id', 'app1');
-    app1.setAttribute('class', 'text-center');
-    app1.innerHTML = 'CHICKEN';
-    list.appendChild(app1);
+  const app1 = document.createElement('h1');
+  app1.setAttribute('id', 'app1');
+  app1.setAttribute('class', 'text-center');
+  app1.innerHTML = 'CHICKEN';
+  item1Container.appendChild(app1);
 
-    const appParagraph1 = document.createElement('p');
-    appParagraph1.setAttribute('class', 'my-3 pb-3');
-    appParagraph1.innerHTML = 'We use ALWAYS FRESH, NEVER EVER FROZEN® premium chicken breast tenderloins to make the most tender chicken fingers possible.';
-    list.appendChild(appParagraph1);
+  const appParagraph1 = document.createElement('p');
+  appParagraph1.setAttribute('class', 'my-3 pb-3');
+  appParagraph1.innerHTML = 'We use ALWAYS FRESH, NEVER EVER FROZEN® premium chicken breast tenderloins to make the most tender chicken fingers possible.';
+  item1Container.appendChild(appParagraph1);
 
-    const app2 = document.createElement('h1');
-    app2.setAttribute('id', 'app2');
-    app2.setAttribute('class', 'text-center');
-    app2.innerHTML = 'BARBECUE';
-    list.appendChild(app2);
+  const appImage1 = document.createElement('div');
+  appImage1.setAttribute('id', 'app1');
+  appImage1.style.backgroundImage = `url('imgs/chickenimg.jpg')`;
+  item1Container.appendChild(appImage1);
 
-    const appParagraph2 = document.createElement('p');
-    appParagraph2.setAttribute('class', 'my-3 pb-3');
-    appParagraph2.innerHTML = 'Full Rack. Slow-cooked to fall off the bone tenderness. Slathered with your choice of sauce: - Spicy Texas BBQ sauce - Honey BBQ sauce';
-    list.appendChild(appParagraph2);
+  appList.appendChild(item1container);
 
-    const app3 = document.createElement('h1');
-    app3.setAttribute('id', 'app3');
-    app3.setAttribute('class', 'text-center');
-    app3.innerHTML = 'FISH';
-    list.appendChild(app3);
+  const item2Container = document.createElement('div');
+  item2Container.setAttribute('class', 'item-container');
 
-    const appParagraph3 = document.createElement('p');
-    appParagraph3.setAttribute('class', 'my-3 pb-3');
-    appParagraph3.innerHTML = 'Blackened salmon fillet grilled to perfection. 6 oz. Served with your choice of two sides.';
-    list.appendChild(appParagraph3);
+  const app2 = document.createElement('h1');
+  app2.setAttribute('id', 'app2');
+  app2.setAttribute('class', 'text-center');
+  app2.innerHTML = 'BARBECUE';
+  item1Container.appendChild(app2);
 
-    const appImage1 = document.createElement('div');
-    appImage1.setAttribute('id', 'app1');
-    appImage1.style.backgroundImage = `url('imgs/chickenimg.jpg')`;
-    imagesContainer.appendChild(appImage1);
+  const appParagraph2 = document.createElement('p');
+  appParagraph2.setAttribute('class', 'my-3 pb-3');
+  appParagraph2.innerHTML = 'Full Rack. Slow-cooked to fall off the bone tenderness. Slathered with your choice of sauce: - Spicy Texas BBQ sauce - Honey BBQ sauce';
+  item1Container.appendChild(appParagraph2);
 
-    const appImage2 = document.createElement('div');
-    appImage2.setAttribute('id', 'app2');
-    appImage2.style.backgroundImage = `url('imgs/barbecueimg.jpg')`;
-    imagesContainer.appendChild(appImage2);
+  const appImage2 = document.createElement('div');
+  appImage2.setAttribute('id', 'app1');
+  appImage2.style.backgroundImage = `url('imgs/barbecueimg.jpg')`;
+  item1Container.appendChild(appImage2);
 
-    const appImage3 = document.createElement('div');
-    appImage3.setAttribute('id', 'app3');
-    appImage3.style.backgroundImage = `url('imgs/fishimg.jpg')`;
-    imagesContainer.appendChild(appImage3);
+  appList.appendChild(item1container);
+  
+  const item3Container = document.createElement('div');
+  item3Container.setAttribute('class', 'item-container');
 
-    return list;
-  };
+  const app3 = document.createElement('h1');
+  app3.setAttribute('id', 'app3');
+  app3.setAttribute('class', 'text-center');
+  app3.innerHTML = 'BARBECUE';
+  item1Container.appendChild(app3);
+
+  const appParagraph3 = document.createElement('p');
+  appParagraph3.setAttribute('class', 'my-3 pb-3');
+  appParagraph3.innerHTML = 'Full Rack. Slow-cooked to fall off the bone tenderness. Slathered with your choice of sauce: - Spicy Texas BBQ sauce - Honey BBQ sauce';
+  item1Container.appendChild(appParagraph3);
+
+  const appImage3 = document.createElement('div');
+  appImage3.setAttribute('id', 'app3');
+  appImage3.style.backgroundImage = `url('imgs/fishimg.jpg')`;
+  item1Container.appendChild(appImage3);
+
+  appList.appendChild(item1container);
 
   appContent.appendChild(appHeader);
   appContent.appendChild(appList);
   appContent.appendChild(imagesContainer);
-  appContent.appendChild(appItems());
+  appContent.appendChild(appItens());
 
   return appContent;
+
 };
 
 export default Appitizers;
+
+document.body.appendChild(Appitizers());
+
